04ffae7d2f017a1b57f19f3b3a78b33d3582b7f8
Doug Popadince <gsmatth@icloud.com>
2017-02-06 09:45:26 -0700

    added 'let' to getFileContent for loop to ensure in order execution

--- lab-doug/test/get-file-contents-test.js
+++ lab-doug/test/get-file-contents-test.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const assert = require('assert');
+const mocha = require('mocha');
+
+describe('get-file-contents module, ', function(){
+  describe('with valid files array passed as argument.', function(){
+    it('should console.log 4, 8byte lines', function(){
+      assert()
+    })
+  })
+})
